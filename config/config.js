0c14edf6378093aaeb82fcd5563ae2693c7407c4
Garam Yoon <rkfka9536@gmail.com>
2025-07-24 08:17:44 -0400

    changed mongodb string

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI ||
-        'mongodb+srv://rkfka9536:dbs3508!@cluster.vmhdsec.mongodb.net/?retryWrites=true&w=majority' ||
+        'mongodb+srv://rkfka9536:dbs3508%21@cluster.vmhdsec.mongodb.net/test?retryWrites=true&w=majority' ||
         'mongodb://' + (process.env.IP || 'localhost') + ':' +
         (process.env.MONGO_PORT || '27017') +
         '/mernproject'
